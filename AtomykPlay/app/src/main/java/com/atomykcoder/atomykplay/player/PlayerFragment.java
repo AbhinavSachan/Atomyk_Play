4740cfb06c995fb04be66c75b8493fcc4a5c2814
Akashi <akashi.gamedev@gmail.com>
2022-10-04 17:18:32 +0530

    Fixed Bug : Shuffle
    shuffle list was created with global scope. meaning it would be created once and everytime we click on shuffle icon, it would add musiclist.size() numbers into it,
    example - first click - 20 numbers
    second click - 40 numbers
    third click - 60 numbers
    
    Solution :
    moved shufflelist inside shuffle function to limit it's scope

--- AtomykPlay/app/src/main/java/com/atomykcoder/atomykplay/player/PlayerFragment.java
+++ AtomykPlay/app/src/main/java/com/atomykcoder/atomykplay/player/PlayerFragment.java
@@ -69,7 +69,7 @@ public class PlayerFragment extends Fragment implements SeekBar.OnSeekBarChangeL
     final private CountDownTimer[] countDownTimer = new CountDownTimer[1];
     private int resumePosition = -1;
     private StorageUtil storageUtil;
-    private ArrayList<Integer> shuffleList = new ArrayList<>();
+
     //setting up mini player layout
     //calling it from service when player is prepared and also calling it in this fragment class
     //to set it on app start ☺
@@ -409,20 +409,17 @@ public class PlayerFragment extends Fragment implements SeekBar.OnSeekBarChangeL
     }
 
     private void shuffleList() {
-
         //shuffle list program
         if (storageUtil.loadShuffle().equals("no_shuffle")) {
             shuffleImg.setImageResource(R.drawable.ic_shuffle);
             storageUtil.saveShuffle("shuffle");
 
+            ArrayList<Integer> shuffleList = new ArrayList<>();
             ArrayList<MusicDataCapsule> musicList = storageUtil.loadMusicList();
-
-
-            for(int i = 0; i < musicList.size(); i++){
-                shuffleList.add(i);
-            }
+            for(int i = 0; i < musicList.size(); i++){ shuffleList.add(i); }
             Collections.shuffle(shuffleList);
             storageUtil.saveShuffleIndexList(shuffleList);
+
         } else if (storageUtil.loadShuffle().equals("shuffle")) {
             shuffleImg.setImageResource(R.drawable.ic_shuffle_empty);
             storageUtil.saveShuffle("no_shuffle");
